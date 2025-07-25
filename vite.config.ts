1f3402e97e02597c437ab450229cdee5f57d9988
Nathan Van Bignoot <nathan.vanbignoot@icloud.com>
2025-07-25 17:02:43 +0200

    refactor: suppression des averstissements lors de la construction de l'application

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				sourcemap: false,
+			},
+			onwarn(warning, warn) {
+				if (warning.code === 'SOURCEMAP_ERROR') return;
+				warn(warning);
+			},
+		},
+	},
 });
